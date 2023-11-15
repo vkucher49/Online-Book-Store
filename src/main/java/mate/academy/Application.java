379652f04ccbd02a7478aeb679ca6396562d6cca
Vlad Kucher <kuchervlad49@gmail.com>
2023-11-15 17:20:01 +0200

    v2

--- src/main/java/mate/academy/Application.java
+++ src/main/java/mate/academy/Application.java
@@ -1,13 +1,37 @@
 package mate.academy;
 
+import java.math.BigDecimal;
+import mate.academy.model.Book;
+import mate.academy.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
+    @Autowired
+    private BookService bookService;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return new CommandLineRunner() {
+            @Override
+            public void run(String... args) throws Exception {
+                Book book = new Book();
+                book.setTitle("Awesome Book");
+                book.setPrice(BigDecimal.valueOf(100));
+
+                bookService.save(book);
+
+                System.out.println(bookService.findAll());
+            }
+        };
     }
 
 }
