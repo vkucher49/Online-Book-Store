bc9e8ee001efdfdd0566bfd6851c2a9ebfa5fae5
Vlad Kucher <kuchervlad49@gmail.com>
2023-11-16 15:58:37 +0200

    mvn clean package done v3

--- src/main/java/mate/academy/model/Book.java
+++ src/main/java/mate/academy/model/Book.java
@@ -1,8 +1,8 @@
 package mate.academy.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
