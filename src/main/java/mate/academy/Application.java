1a3571bd98c9a912544e14fbfe81dc0042c1b947
Vlad Kucher <kuchervlad49@gmail.com>
2023-11-22 15:53:45 +0200

    mvn clean package done

--- src/main/java/mate/academy/Application.java
+++ src/main/java/mate/academy/Application.java
@@ -2,9 +2,8 @@ package mate.academy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(scanBasePackages = "mate.academy")
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
