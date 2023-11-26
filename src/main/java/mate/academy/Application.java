42ef78537153ad1632b52836ab8fa42dd956d966
Vlad Kucher <kuchervlad49@gmail.com>
2023-11-26 16:21:52 +0200

    rollback to the previous commit, to merge this to main

--- src/main/java/mate/academy/Application.java
+++ src/main/java/mate/academy/Application.java
@@ -2,7 +2,6 @@ package mate.academy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(scanBasePackages = "mate.academy")
 public class Application {
