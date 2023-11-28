2e6bbbc5ac58246deb74ba943696d40f2ff4991f
Vlad Kucher <kuchervlad49@gmail.com>
2023-11-28 18:36:59 +0200

    DataValidation, GlobalExceptionHandler hw done v1

--- src/main/java/mate/academy/controller/BookController.java
+++ src/main/java/mate/academy/controller/BookController.java
@@ -1,7 +1,7 @@
 package mate.academy.controller;
 
-import java.util.List;
 import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import mate.academy.dto.BookDto;
 import mate.academy.dto.BookSearchParameters;
--- src/main/java/mate/academy/dto/CreateBookRequestDto.java
+++ src/main/java/mate/academy/dto/CreateBookRequestDto.java
@@ -1,8 +1,8 @@
 package mate.academy.dto;
 
-import java.math.BigDecimal;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
