93bf58e0b4100ffcaea40d7a406b97d63628f410
Vlad Kucher <kuchervlad49@gmail.com>
2023-12-05 20:01:41 +0200

    test

--- src/main/java/mate/academy/controller/AuthController.java
+++ src/main/java/mate/academy/controller/AuthController.java
@@ -2,8 +2,11 @@ package mate.academy.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import mate.academy.dto.user.UserLoginRequestDto;
+import mate.academy.dto.user.UserLoginResponseDto;
 import mate.academy.dto.user.UserRegistrationRequestDto;
 import mate.academy.dto.user.UserResponseDto;
+import mate.academy.security.AuthenticationService;
 import mate.academy.service.UserService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,9 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/auth")
 public class AuthController {
     private final UserService userService;
+    private final AuthenticationService authenticationService;
 
     @PostMapping("/register")
     public UserResponseDto register(@RequestBody @Valid UserRegistrationRequestDto requestDto) {
         return userService.register(requestDto);
+    }
+
+    @PostMapping("/login")
+    public UserLoginResponseDto login(@RequestBody UserLoginRequestDto requestDto) {
+        return authenticationService.authenticate(requestDto);
     }
 }
---
+++ src/main/java/mate/academy/dto/user/UserLoginRequestDto.java
@@ -0,0 +1,16 @@
+package mate.academy.dto.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class UserLoginRequestDto {
+    @Email
+    @NotNull
+    private String email;
+    @NotNull
+    @Size(min = 6, max = 100)
+    private String password;
+}
